cc16da03c825dd4f7011f36471a301293d263e01
Semir N <semirnik2@gmail.com>
2018-03-16 13:43:30 -0400

    Create for_loop_time.js

---
+++ for_loop_time.js
@@ -0,0 +1,9 @@
+//THIS IS A TIMED FOR LOOP
+
+for (var i=0; i<i; i++) {
+  (function (i) {
+    setTimeout(function () {
+    console.log(i)
+  	},750*i); //<--- in mms
+  })(i);
+};// End of for loop
