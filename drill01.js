914ab295ebb4f35b4bc26f0a42140147991ac93d
Semir N <semirnik2@gmail.com>
2019-06-24 18:19:30 -0400

    Update drill01.js

--- drill01.js
+++ drill01.js
@@ -8,11 +8,12 @@
   // Reverse the string and check if matches, but the string has to be in array
   // EXAMPLE: "meow" => ['m','e','o','w'] => reverse => and join the array 
   const reverseArray= str.split('').reverse().join('');
-  if(str === reverseArray){
-    return true;
-  }else{
-    return false;
-  }
+  //   if(str === reverseArray){
+  //     return true;
+  //   }else{
+  //     return false;
+  //   }
+  return str === reverseArray ? true: false;
 }
 
 let word = "meow";
