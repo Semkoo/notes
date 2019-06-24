f4b7f3f102381b33997ffc3b9d653894015598ab
Semir N <semirnik2@gmail.com>
2019-06-24 18:29:20 -0400

    Update drill01.js

--- drill01.js
+++ drill01.js
@@ -4,6 +4,9 @@
  
  isPalindrome = (str) =>{
   // TODO:: VALIDATION 
+  // Base Case
+  // Only care about strings so if its null or Integer or underfind
+  
   
   // Reverse the string and check if matches, but the string has to be in array
   // EXAMPLE: "meow" => ['m','e','o','w'] => reverse => and join the array 
@@ -13,7 +16,8 @@
   //   }else{
   //     return false;
   //   }
-  return str === reverseArray ? true: false;
+  //   return str === reverseArray ? true: false;
+  return str === reverseArray;
 }
 
 let word = "meow";
