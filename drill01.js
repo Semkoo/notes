7e9c5381828c19702d1551e8af688e0da25c5330
Semir N <semirnik2@gmail.com>
2019-06-24 18:40:33 -0400

    Update drill01.js

--- drill01.js
+++ drill01.js
@@ -6,7 +6,9 @@
   // TODO:: VALIDATION 
   // Base Case
   // Only care about strings so if its null or Integer or underfind
-  
+  if(input === null || input === undefined || String(str).length === 0 ){
+     throw new Error("Undefiend values")
+  }
   
   // Reverse the string and check if matches, but the string has to be in array
   // EXAMPLE: "meow" => ['m','e','o','w'] => reverse => and join the array 
@@ -25,10 +27,15 @@ let word = ['bob', 'xguvv']
 
 
 for(let str of words){
+ try{
  if(isPalindrome(str)){
    console.log(`${str} is palindrome`);
  }else{
    console.log(`${str} is not palindrome `);
+ }
+ }
+ catch {
+   console.log("Error occured");
  }
 }
 
