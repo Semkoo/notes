f93a53af6679870b5489ff27cf1054c0280aa2c0
Semir N <semirnik2@gmail.com>
2019-07-08 18:37:37 -0400

    Update max-profit.js

--- max-profit.js
+++ max-profit.js
@@ -1,5 +1,11 @@
 /** Maximum Profit
 * Imagine your in the stock trading line of business. So, everyday the stock price change
 * [80, 90, 85, 50, 120, 75, 50, 30, 110]
+* ['Monday', 'Tuesday' etc]
 *  Now, we're tasked with finding out the maximum profit we could get if we were to buy one day and see another 
+*
+* What would you ask? 
+* How many prices/days? 
+* A: unbounded
+* 
 **/
