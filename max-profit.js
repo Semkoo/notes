f84c5d7e13dec9b02efde8307bd7deeafa7432bc
Semir N <semirnik2@gmail.com>
2019-07-08 18:22:06 -0400

    Update max-profit.js

--- max-profit.js
+++ max-profit.js
@@ -1,1 +1,5 @@
-// Maximum Profit
+/** Maximum Profit
+* Imagine your in the stock trading line of business. So, everyday the stock price change
+* [80, 90, 85, 50, 120, 75, 50, 30, 110]
+*  Now, we're tasked with finding out the maximum profit we could get if we were to buy one day and see another 
+**/
